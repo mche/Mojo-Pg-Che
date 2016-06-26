57fbf95507959803199cab7f6072bb1b0b358d6e
Mikhail Che <ernesto.chege@yandex.ru>
2016-06-27 00:31:33 +0500

    1

---
+++ lib/Mojo/Pg/Che.pm
@@ -0,0 +1,65 @@
+package Mojo::Pg::Che;
+
+use Mojo::Base 'Mojo::Pg';
+
+=pod
+
+=encoding utf-8
+
+Доброго всем
+
+=head1 Mojo::Pg::Che
+
+¡ ¡ ¡ ALL GLORY TO GLORIA ! ! !
+
+=head1 NAME
+
+Mojo::Pg::Che - mix of parent Mojo::Pg and DBI.pm
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Mojo::Pg::Che;
+
+    my $foo = Mojo::Pg::Che->new();
+    ...
+
+=head1 AUTHOR
+
+Михаил Че (Mikhail Che), C<< <mche[-at-]cpan.org> >>
+
+=head1 BUGS / CONTRIBUTING
+
+Please report any bugs or feature requests at L<https://github.com/mche/Mojo-Pg-Che/issues>. Pull requests also welcome.
+
+=head1 COPYRIGHT
+
+Copyright 2016 Mikhail Che.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+
+
+
+
+
+
+=cut
+
+
+
+
+1; # End of Mojo::Pg::Che
